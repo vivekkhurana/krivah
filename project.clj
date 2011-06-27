a2fc68976c4757276f8cd00bfc53a72148a6188c
Vivek Khurana <hiddenharmony@gmail.com>
2011-06-27 18:03:15 +0530

    First commit

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject krivah "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [ring/ring-core "0.3.7"]
+                 [org.ccil.cowan.tagsoup/tagsoup "1.2"]
+                 [ring/ring-jetty-adapter "0.3.7"]
+                 [ring-json-params "0.1.0"]
+                 [ring/ring-devel "0.3.7"]
+                 [congomongo "0.1.3-SNAPSHOT"]
+                 ]
+            
+)
